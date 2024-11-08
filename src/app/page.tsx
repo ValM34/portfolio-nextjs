c165bb3a660b249161ed72cdc89275e7e462da77
valentin moreau <valentin.moreau34750@gmail.com>
2024-11-08 02:10:43 +0100

    wip

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import Link from "next/link";
 import {
   ArrowLongDownIcon,
+  ArrowLongUpIcon,
 } from "@heroicons/react/24/outline";
 import Tag from "@/app/components/components/tag";
 
@@ -32,7 +33,7 @@ export default function Home() {
           </div>
           <div className="flex flex-col items-center">
             <Link
-              href="/#project_1"
+              href="/#project_todo_list"
               className="flex mb-4 mx-auto bg-gradient-to-b from-amber-400 to-amber-500 rounded-full py-4 px-6 text-white text-2xl transition-transform hover:scale-105 w-fit"
             >
               <div className="mx-4 font-medium">Découvrir mes réalisations</div>
@@ -40,61 +41,10 @@ export default function Home() {
             <ArrowLongDownIcon className="size-8 mt-1 text-white animate-bounce" />
           </div>
         </div>
-
-        {/* <div id="resume" className="w-full p-6">
-        <h2 className="text-4xl font-bold mb-4">
-          Expériences professionnelles
-        </h2>
-        <ul>
-          {experience.map((item) => {
-            return (
-              <li key={item.id} className="flex flex-col">
-                <b>
-                  {item.company} | {item.date}
-                </b>
-                <i className="font-semibold">{item.job}</i>
-                <ul>
-                  {item.skills.map((skill) => {
-                    return (
-                      <li className="list-disc" key={skill}>
-                        {skill}
-                      </li>
-                    );
-                  })}
-                </ul>
-              </li>
-            );
-          })}
-        </ul>
       </div>
 
-      <div className="w-full p-6">
-        <h2 className="text-4xl font-bold mb-4">Formation</h2>
-        <ul>
-          {education.map((item) => {
-            return (
-              <li key={item.id} className="flex flex-col">
-                <b>
-                  {item.organism} | {item.date}
-                </b>
-                <i className="font-semibold">{item.examObtained}</i>
-                <ul>
-                  {item.skills.map((skill) => {
-                    return (
-                      <li className="list-disc" key={skill}>
-                        {skill}
-                      </li>
-                    );
-                  })}
-                </ul>
-              </li>
-            );
-          })}
-        </ul>
-      </div> */}
-      </div>
       <div
-        id="project_1"
+        id="project_todo_list"
         className="bg-gradient-to-r from-amber-500 to-amber-300"
       >
         <div className="flex flex-col justify-between max-w-6xl mx-auto min-h-screen pt-12">
@@ -133,10 +83,17 @@ export default function Home() {
                 </Link>
                 <Link
                   href="https://todolist-valentin-moreau.vercel.app/"
-                  className="flex w-fit bg-red-600 rounded-full py-4 px-4 text-white text-xl transition-transform hover:scale-105"
+                  className="flex w-fit bg-red-600 rounded-full py-4 px-4 text-white text-xl transition-transform hover:scale-105 mr-4"
                   target="_blank"
                 >
                   <div className="mx-4 font-medium">Voir le site</div>
+                </Link>
+                <Link
+                  href="#"
+                  className="flex w-fit bg-red-600 rounded-full py-4 px-4 text-white text-xl transition-transform hover:scale-105"
+                  target="_blank"
+                >
+                  <div className="mx-4 font-medium">Plus d&apos;infos sur le projet</div>
                 </Link>
               </div>
             </div>
@@ -148,17 +105,164 @@ export default function Home() {
               className="ml-4 h-full border-amber-500"
             />
           </div>
-          <Link
-            href="/#"
-            className="flex mb-4 mx-auto bg-gradient-to-b from-indigo-400 to-indigo-500 rounded-full py-4 px-6 text-white text-2xl transition-transform hover:scale-105"
-          >
-            <div className="mx-4 font-medium">Plus d&apos;infos sur le projet</div>
-          </Link>
+          <div className="flex flex-col items-center">
+            <Link
+              href="/#project_portfolio"
+              className="flex mb-4 mx-auto bg-gradient-to-b from-indigo-400 to-indigo-500 rounded-full py-4 px-6 text-white text-2xl transition-transform hover:scale-105 w-fit"
+            >
+              <div className="mx-4 font-medium">Projet suivant</div>
+            </Link>
+            <ArrowLongDownIcon className="size-8 mt-1 text-white animate-bounce" />
+          </div>
         </div>
       </div>
 
-      <div className="bg-gradient-to-r from-amber-500 to-amber-300">
-        <h2>Projet Todo list en NextJs</h2>
+      <div
+        id="project_portfolio"
+        className="bg-gradient-to-r from-indigo-500 to-indigo-300"
+      >
+        <div className="flex flex-col justify-between max-w-6xl mx-auto min-h-screen pt-12">
+          <div></div>
+          <div className="flex">
+            <Image
+              src="/checklist.png"
+              alt="Photo de Valentin"
+              width={300}
+              height={320}
+              className="mr-4 h-full border-amber-500"
+            />
+            <div className="flex flex-col justify-center">
+              <h2 className="text-4xl font-bold mb-4">Portfolio Next.js & Strapi</h2>
+              <div className="mb-3">
+                <span className="mr-2 mb-2 inline-block"><Tag>NextJs</Tag></span>
+                <span className="mr-2 mb-2 inline-block"><Tag>Prisma</Tag></span>
+                <span className="mr-2 mb-2 inline-block"><Tag>TypeScript</Tag></span>
+                <span className="mr-2 mb-2 inline-block"><Tag>Yup</Tag></span>
+                <span className="mr-2 mb-2 inline-block"><Tag>Formik</Tag></span>
+                <span className="mr-2 mb-2 inline-block"><Tag>TailwindCss/UI</Tag></span>
+                <span className="mr-2 mb-2 inline-block"><Tag>jsonwebtoken/jose/bcrypt</Tag></span>
+                <span className="mr-2 mb-2 inline-block"><Tag>Jest</Tag></span>
+              </div>
+              <p className="text-2xl">
+                Ayant terminé ma formation <b>Javascript/React</b> fin 2024,
+                j&apos;ai souhaité me former sur le framework <b>Next.Js</b>.
+                J&apos;ai donc choisi de faire un <b>projet de Todo list</b>,
+                mais ne vous y trompez pas:{" "}
+                <b>
+                  ce n&apos;est pas une todo list simple réalisable en une
+                  après-midi!
+                </b>{" "}
+                Elle est très complète et m&apos;a apporté une <b>expérience significative</b> en <b>Next.js!</b>
+              </p>
+              <div className="flex justify-start mt-4">
+                <Link
+                  href="https://github.com/ValM34/todolist-nextjs"
+                  className="flex w-fit bg-github rounded-full py-4 px-4 text-white text-xl transition-transform hover:scale-105 mr-4"
+                  target="_blank"
+                >
+                  <div className="mx-4 font-medium">GitHub</div>
+                </Link>
+                <Link
+                  href="https://todolist-valentin-moreau.vercel.app/"
+                  className="flex w-fit bg-red-600 rounded-full py-4 px-4 text-white text-xl transition-transform hover:scale-105 mr-4"
+                  target="_blank"
+                >
+                  <div className="mx-4 font-medium">Voir le site</div>
+                </Link>
+                <Link
+                  href="#"
+                  className="flex w-fit bg-red-600 rounded-full py-4 px-4 text-white text-xl transition-transform hover:scale-105"
+                  target="_blank"
+                >
+                  <div className="mx-4 font-medium">Plus d&apos;infos sur le projet</div>
+                </Link>
+              </div>
+            </div>
+          </div>
+          <div className="flex flex-col items-center">
+            <Link
+              href="/#project_e_commerce_tout_un_concept"
+              className="flex mb-4 mx-auto bg-gradient-to-b from-amber-400 to-amber-500 rounded-full py-4 px-6 text-white text-2xl transition-transform hover:scale-105 w-fit"
+            >
+              <div className="mx-4 font-medium">Projet suivant</div>
+            </Link>
+            <ArrowLongDownIcon className="size-8 mt-1 text-white animate-bounce" />
+          </div>
+        </div>
+      </div>
+
+      <div
+        id="project_e_commerce_tout_un_concept"
+        className="bg-gradient-to-r from-amber-500 to-amber-300"
+      >
+        <div className="flex flex-col justify-between max-w-6xl mx-auto min-h-screen pt-12">
+          <div></div>
+          <div className="flex">
+            <div className="flex flex-col justify-center">
+              <h2 className="text-4xl font-bold mb-4">E-commerce Next.js & Strapi</h2>
+              <div className="mb-3">
+                <span className="mr-2 mb-2 inline-block"><Tag>NextJs</Tag></span>
+                <span className="mr-2 mb-2 inline-block"><Tag>Prisma</Tag></span>
+                <span className="mr-2 mb-2 inline-block"><Tag>TypeScript</Tag></span>
+                <span className="mr-2 mb-2 inline-block"><Tag>Yup</Tag></span>
+                <span className="mr-2 mb-2 inline-block"><Tag>Formik</Tag></span>
+                <span className="mr-2 mb-2 inline-block"><Tag>TailwindCss/UI</Tag></span>
+                <span className="mr-2 mb-2 inline-block"><Tag>jsonwebtoken/jose/bcrypt</Tag></span>
+                <span className="mr-2 mb-2 inline-block"><Tag>Jest</Tag></span>
+              </div>
+              <p className="text-2xl">
+                Ayant terminé ma formation <b>Javascript/React</b> fin 2024,
+                j&apos;ai souhaité me former sur le framework <b>Next.Js</b>.
+                J&apos;ai donc choisi de faire un <b>projet de Todo list</b>,
+                mais ne vous y trompez pas:{" "}
+                <b>
+                  ce n&apos;est pas une todo list simple réalisable en une
+                  après-midi!
+                </b>{" "}
+                Elle est très complète et m&apos;a apporté une <b>expérience significative</b> en <b>Next.js!</b>
+              </p>
+              <div className="flex justify-start mt-4">
+                <Link
+                  href="https://github.com/ValM34/todolist-nextjs"
+                  className="flex w-fit bg-github rounded-full py-4 px-4 text-white text-xl transition-transform hover:scale-105 mr-4"
+                  target="_blank"
+                >
+                  <div className="mx-4 font-medium">GitHub</div>
+                </Link>
+                <Link
+                  href="https://todolist-valentin-moreau.vercel.app/"
+                  className="flex w-fit bg-red-600 rounded-full py-4 px-4 text-white text-xl transition-transform hover:scale-105 mr-4"
+                  target="_blank"
+                >
+                  <div className="mx-4 font-medium">Voir le site</div>
+                </Link>
+                <Link
+                  href="#"
+                  className="flex w-fit bg-red-600 rounded-full py-4 px-4 text-white text-xl transition-transform hover:scale-105"
+                  target="_blank"
+                >
+                  <div className="mx-4 font-medium">Plus d&apos;infos sur le projet</div>
+                </Link>
+              </div>
+            </div>
+            <Image
+              src="/checklist.png"
+              alt="Photo de Valentin"
+              width={300}
+              height={320}
+              className="ml-4 h-full border-amber-500"
+            />
+          </div>
+          <div className="flex flex-col items-center">
+            <ArrowLongUpIcon className="size-8 mt-1 text-white animate-bounce" />
+            <Link
+              href="/#main_header"
+              className="flex mb-4 mx-auto bg-gradient-to-b from-indigo-400 to-indigo-500 rounded-full py-4 px-6 text-white text-2xl transition-transform hover:scale-105 w-fit"
+            >
+              <div className="mx-4 font-medium">Remonter la page</div>
+            </Link>
+          </div>
+        </div>
       </div>
     </>
   );
