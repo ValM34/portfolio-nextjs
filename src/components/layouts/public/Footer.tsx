ebdc6a08a709caab368b11db959c74d84252f25b
valentin moreau <valentin.moreau34750@gmail.com>
2024-12-03 23:44:22 +0100

    fix date footer

--- src/components/layouts/public/Footer.tsx
+++ src/components/layouts/public/Footer.tsx
@@ -2,7 +2,8 @@ export default function Footer() {
   return (
     <footer className="bg-gradient-to-r from-slate-950 to-slate-900 text-slate-300 border-t border-slate-700">
       <div className="text-center p-4">
-        © 2024 Copyright: Valentin Moreau. Tous droits réservés.
+        © {new Date().getFullYear()} Copyright: Valentin Moreau. Tous droits
+        réservés.
       </div>
     </footer>
   );
