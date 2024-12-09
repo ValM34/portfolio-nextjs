479ad16395715363a9b81800b400f1c958719a5b
valentin moreau <valentin.moreau34750@gmail.com>
2024-12-09 23:38:52 +0100

    hotfix

--- src/app/projects-presentation/[slug]/layout.tsx
+++ src/app/projects-presentation/[slug]/layout.tsx
@@ -1,6 +1,6 @@
 export default function Layout({ children } : { children: React.ReactNode }) {
   return (
-    <div id="override_tailwind_for_strapi_rich_text" className="bg-gradient-to-r from-slate-950 to-slate-900 text-slate-200 min-h-[calc(100vh-65px-57px)] flex flex-col items-center justify-center px-6 sm:px-12 py-12">
+    <div id="override_tailwind_for_wordpress_content" className="bg-gradient-to-r from-slate-950 to-slate-900 text-white min-h-[calc(100vh-65px-57px)] flex flex-col items-center justify-center px-6 sm:px-12 py-12">
       <div className="max-w-3xl">
         {children}
       </div>
