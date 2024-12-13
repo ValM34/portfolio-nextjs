5153cb04100eaf010f6f21d508b83dcfebafdcb2
valentin moreau <valentin.moreau34750@gmail.com>
2024-12-13 12:00:30 +0100

    feat(magicUI): implement

--- src/app/HeroSection.tsx
+++ src/app/HeroSection.tsx
@@ -1,7 +1,12 @@
-import { ArrowLongDownIcon } from "@heroicons/react/24/outline";
-import CtaLink from "@/components/components/links/CTALink";
+import { ArrowLongDownIcon } from '@heroicons/react/24/outline';
+import { cn } from '@/lib/utils';
+import GridPattern from '@/components/ui/grid-pattern';
+import ShimmerButton from '@/components/ui/shimmer-button';
+import { useRouter } from 'next/navigation';
 
 export default function HeroSection() {
+  const router = useRouter();
+
   return (
     <div className="bg-gradient-to-r from-slate-950 to-slate-900 text-slate-300 min-h-[calc(100vh-65px)] flex flex-col items-center justify-center px-4">
       <div className="flex flex-col justify-center items-center max-w-6xl mx-auto pt-12 h-full">
@@ -12,12 +17,26 @@ export default function HeroSection() {
               développeur web
             </div>
           </h1>
+          <GridPattern
+            width={40}
+            height={40}
+            x={-1}
+            y={-1}
+            className={cn(
+              '[mask-image:linear-gradient(to_top_right,white,transparent,transparent)] '
+            )}
+          />
         </div>
-        <div className="mb-4">
-          <CtaLink url={'/#projects_list'} theme={'primary'}>
+        <ShimmerButton
+          background="orange"
+          shimmerColor="#0f172a"
+          className="shadow-2xl shadow-orange-950"
+          onClick={() => router.push('/#projects_list')}
+        >
+          <span className="whitespace-pre-wrap text-center text-sm font-medium leading-none tracking-tight text-slate-900 lg:text-lg">
             Mes projets
-          </CtaLink>
-        </div>
+          </span>
+        </ShimmerButton>
       </div>
       <ArrowLongDownIcon className="w-8 h-8 animate-bounce text-slate-300 absolute bottom-0" />
     </div>
